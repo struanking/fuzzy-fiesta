09b8187d0b5816abf7de5485f28b70ed1a9c3b35
Struan King <struan.king@gmail.com>
2018-07-18 17:08:13 +0100

    Adds Holen to fetch remote data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import Holen from 'holen';
+import React, { Fragment } from 'react';
 import { injectGlobal } from 'styled-components';
 import EnergyUsage from './components/EnergyUsage';
 import Layout from './components/Layout';
 import MeterReadings from './components/MeterReadings';
-import meterReadingsData from './data/meterReadingsSample.json';
 import globalStyles from './globalStyles';
 
 /* eslint-disable-next-line no-unused-expressions */
 injectGlobal`${globalStyles}`;
 
+const handleResponse = data => data.electricity;
+
 export default () => (
   <Layout>
-    <EnergyUsage meterReadings={meterReadingsData.electricity} />
-    <MeterReadings meterReadings={meterReadingsData.electricity} />
+    <Holen transformResponse={handleResponse} url="https://storage.googleapis.com/bulb-interview/meterReadingsReal.json">
+      {({ data, fetching }) => (
+        fetching
+          ? <p>Loading meter readings...</p>
+          : (
+            <Fragment>
+              <EnergyUsage meterReadings={data} />
+              <MeterReadings meterReadings={data} />
+            </Fragment>
+          )
+        )
+      }
+    </Holen>
   </Layout>
 );
